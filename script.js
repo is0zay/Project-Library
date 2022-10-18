c544fb61b871f130d28fb27a63fbc1ca2a369cb6
Isaiah <ieshembo@alumni.uncg.edu>
2022-10-17 20:10:14 -0400

    still working

--- script.js
+++ script.js
@@ -10,25 +10,110 @@ document.querySelector('.close').addEventListener('click', ()=> {
 });
 */
 
+// Main Div section of books
 const books = document.querySelector('.books'); 
-const myLibrary = [
-    {
-        title: 'Book1',
-        author: 'Me',
-        pages: '824',
-        read: true,
+const addBook = document.querySelector('.add-book');
+const modal = document.querySelector('#modal');
+const closeModal = document.querySelector('.close');
 
-    },
-    {
-        title:"Book2",
-        author:"me",
-        pages:"500",
-        read: false,
+window.addEventListener('click', (e) => {
+    if (e.target == modal){
+        modal.style.display = 'none';
     }
-];
+});
 
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
 
+addBook.addEventListener('click', () => {
+    modal.style.display = 'block';
+    document.querySelector('.form-title').textContent = "Add Book";
+    document.querySelector('.formButton').textContent = "Add";
+});
 
+function createEditIcon(book) {
+    
+    const editIcon = document.createElement('img');
+    editIcon.src = '../img/pencil.svg';
+    editIcon.setAttribute('class', 'edit-icon')
+    editIcon.addEventListener('click', (e) => {
+         console.log(book);
+    });
+    return editIcon
+}
+
+/*
+function createIcons() {
+    const div = createBookElement('div', '', 'icons');
+    const icon1 = document.createElement('img');
+    icon1.src = '../img/icon1.svg';
+
+    const icon2 = document.createElement('img');
+    icon1.src = '../img/icon2.svg';
+
+    const icon3 = document.createElement('img');
+    icon1.src = '../img/icon3.svg';
+
+    div.appendChild(icon1);
+    div.appendChild(icon2);
+    div.appendChild(icon3);
+    
+    return div;
+}
+
+*/
+
+function Book(title,author,pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = Math.floor(Math.random * 10000000000)
+}
+
+function addToLibrary(title, author, pages, read){
+    myLibrary.push(new Book(title,author,pages,read));
+    saveAndRenderBooks();
+}
+
+const addBookForm = document.querySelector('.addBookForm');
+addBookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(e.target);
+    let newB = {};
+    for(let [name,value] of data) {
+        if(name === 'book-read'){
+            newB['book-read'] = true;
+        }else {
+            newB[name] = value || "";
+        }
+    }
+
+    if(!newB['book-read']){
+        newB['book-read'] = false
+    }
+    addToLibrary(
+        newB['title'],
+        newB['author'],
+        newB['pages'],
+        newB['read']
+        );
+    addBookForm.reset();
+    modal.style.display = 'none';
+});
+
+//array of books input
+let myLibrary = [];
+
+function addLocalStorage() {
+    // localStorage => save things in key value pairs - key = library : myLibraary
+    myLibrary = JSON.parse(localStorage.getItem('library')) || [];
+    saveAndRenderBooks();
+};
+
+// function to create html elements with content and classes
 function createBookElement(el, content, className) {
     const element = document.createElement(el);
     element.textContent = content;
@@ -36,6 +121,7 @@ function createBookElement(el, content, className) {
     return element;
 }
 
+// function to create an input with event listener
 function createReadElement(bookItem,book) {
     const read = document.createElement('div');
     read.setAttribute('class', 'book-read');
@@ -46,10 +132,12 @@ function createReadElement(bookItem,book) {
         if(e.target.checked){
             bookItem.setAttribute('class', 'card book read-checked');
             book.read = true;
+            saveAndRenderBooks();
 
         }else {
             bookItem.setAttribute('class', 'card book read-unchecked');
             book.read = false;
+            saveAndRenderBooks();
             
 
         }
@@ -64,6 +152,12 @@ function createReadElement(bookItem,book) {
     return read;
 }
 
+function deleteBook(index) {
+    myLibrary.splice(index, 1);
+    saveAndRenderBooks();
+};
+
+// function to create book content  on the card
 function createBookItem(book, index) {
     const bookItem = document.createElement('div');
     bookItem.setAttribute('id', index);
@@ -77,18 +171,35 @@ function createBookItem(book, index) {
     bookItem.appendChild(createBookElement('h1', `Pages:` + `${book.pages}`, "book-pages"));
 
     bookItem.appendChild(createReadElement(bookItem,book)); 
+    bookItem.appendChild(createBookElement('button', 'X', 'delete'));
+
+    
+    bookItem.appendChild(createEditIcon(book));
+    // bookItem.appendChild(createIcons());
+
+    bookItem.querySelector('.delete').addEventListener('click', () => {
+        deleteBook(index);
+    });
 
     books.insertAdjacentElement('afterbegin', bookItem);
 
 }
 
+
+// function add books to library array
 function addBookToLibrary() {
+    books.textContent = "";
   myLibrary.map((book,index) => {
     createBookItem(book,index);
   });
 }
 
-addBookToLibrary();
+function saveAndRenderBooks () {
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+    addBookToLibrary();
+}
+
+addLocalStorage();
 
 
 
